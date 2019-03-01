1402c80ea27555120a6a98471060389fc76fedfe
gutsy-robot <agrawalsiddharth12@gmail.com>
2019-03-01 00:43:38 -0500

    todos for general polygon obstacles

--- pydmps/utils.py
+++ pydmps/utils.py
@@ -28,7 +28,7 @@ def get_trajectory(csvfile):
     return path_x, path_y
 
 
-def plot_path(x, y, n_bfs=[30], start=[0, 0], goal=[1, 1]):
+def plot_path(x, y, n_bfs=[30], start=[5.0, 6.0], goal=[5.3, 8.0], obstacles=None):
 
     """
 
@@ -37,6 +37,7 @@ def plot_path(x, y, n_bfs=[30], start=[0, 0], goal=[1, 1]):
     :param n_bfs: array with number of basis functions for the DMP
     :param start: start state for the DMP
     :param goal: (x,y) goal for the DMP
+    :param obstacles: ; list of shapely polygons
     :return: plots for the
 
     """
@@ -50,41 +51,67 @@ def plot_path(x, y, n_bfs=[30], start=[0, 0], goal=[1, 1]):
     plt.plot(x[-1], y[-1], 'bo')
     plt.annotate("f original.", (x[-1], y[-1]))
 
-    # plt.plot(start[0], start[1], 'bo')
-    # plt.annotate("new_st", (start[0], start[1]))
-    # plt.plot(goal[0], goal[1], 'bo')
-    # plt.annotate("new_fin", (goal[0], goal[1]))
+    plt.plot(start[0], start[1], 'bo')
+    plt.annotate("new_st", (start[0], start[1]))
+    plt.plot(goal[0], goal[1], 'bo')
+    plt.annotate("new_fin", (goal[0], goal[1]))
 
     for ii, bfs in enumerate(n_bfs):
         dmp = DMPs_discrete(n_dmps=2, n_bfs=bfs)
 
         dmp.imitate_path(y_des=np.array([x, y]))
-        dmp.y0[0] = x[0]
-        dmp.y0[1] = y[0]
+        dmp.y0[0] = start[0]
+        dmp.y0[1] = start[1]
         # change the scale of the movement
-        dmp.goal[0] = x[-1]
-        dmp.goal[1] = y[-1]
+
+
+        dmp.goal[0] = goal[0]
+        dmp.goal[1] = goal[1]
 
         y_track, dy_track, ddy_track = dmp.rollout()
+        print("f original.", (goal[0], goal[1]))
+        print("the end pt is: ", (y_track[:, 0][-1], y_track[:, 1][-1]))
         plot, = plt.plot(y_track[:, 0], y_track[:, 1])
         plot_paths.append(plot)
-        plt.plot(5.6, 6.6, 'bo')
-        plt.annotate("obstacle1", (5.6, 6.6))
-        plt.plot(4.0, 8.0, 'bo')
-        plt.annotate("obstacle2", (4.0, 8.0))
-    # plt.legend(plot_paths, ['original_path'] + ['%i BFs' % i for i in n_bfs], loc='lower right')
+
+    plt.legend(plot_paths, ['original_path'] + ['%i BFs' % i for i in n_bfs], loc='lower right')
 
     for ii, bfs in enumerate(n_bfs):
         dmp = DMPs_discrete(n_dmps=2, n_bfs=bfs)
 
         dmp.imitate_path(y_des=np.array([x, y]))
-        dmp.y0[0] = x[0]
-        dmp.y0[1] = y[0]
+        dmp.y0[0] = start[0]
+        dmp.y0[1] = start[1]
+
         # change the scale of the movement
-        dmp.goal[0] = x[-1]
-        dmp.goal[1] = y[-1]
+
+        # TODO: apply goal_check and goal_alteration
+        # if obstacles != None:
+        # dmp.goal[0], dmp.goal[1] = check_goal(goal)
+
+        dmp.goal[0] = goal[0]
+        dmp.goal[1] = goal[1]
         print(dmp.y, dmp.dy)
-        y_track, dy_track, ddy_track = dmp.rollout(external_force=avoid_obstacles, obstacles=np.array([[5.6, 6.6]]))
+
+        # TODO: code for plotting shapely polygons.
+
+        obst_y = list(np.linspace(7.7, 8.2, num=20)) + [8.2] * 10 + list(np.linspace(7.7, 8.2, num=20)) + [7.7] * 10
+        obst_x = [5.0] * 20 + list(np.linspace(5.0, 5.25, num=10)) + [5.25] * 20 + list(np.linspace(5.0, 5.25, num=10))
+        obst = []
+        for e in zip(obst_x, obst_y):
+            obst.append(np.asarray(e))
+
+        obst = np.array(obst)
+        print("obstacles are: ", obst)
+        for o in obst:
+            plt.plot(o[0], o[1], 'bo')
+
+        # plt.plot(5.6, 6.6, 'bo')
+        # plt.annotate("obstacle1", (5.6, 6.6))
+        # plt.plot(4.0, 8.0, 'bo')
+        # plt.annotate("obstacle2", (4.0, 8.0))
+
+        y_track, dy_track, ddy_track = dmp.rollout(external_force=avoid_obstacles, obstacles=obst)
         plot, = plt.plot(y_track[:, 0], y_track[:, 1])
         plot_paths.append(plot)
 
@@ -92,9 +119,20 @@ def plot_path(x, y, n_bfs=[30], start=[0, 0], goal=[1, 1]):
     plt.show()
 
 
-def avoid_obstacles(y, dy, goal, obstacles=np.array([[5.6, 6.6], [4.0, 8.0]]), beta=20.0 / np.pi, gamma=100):
+def avoid_obstacles(y, dy, goal, obstacles=np.array([[5.6, 6.6], [4.0, 8.0]]), beta=20.0 / np.pi, gamma=10):
 
-    print("avoid obstacles called..")
+    """
+
+    :param y: position
+    :param dy: velocity
+    :param goal: goal coordinates
+    :param obstacles: list of shapely polygons
+    :param beta: constant
+    :param gamma: constant
+    :return:
+    """
+
+    # print("avoid obstacles called..")
 
     R_halfpi = np.array([[np.cos(np.pi / 2.0), -np.sin(np.pi / 2.0)],
                          [np.sin(np.pi / 2.0), np.cos(np.pi / 2.0)]])
@@ -114,7 +152,11 @@ def avoid_obstacles(y, dy, goal, obstacles=np.array([[5.6, 6.6], [4.0, 8.0]]), b
             # print("phi_dy is: ", phi_dy)
             R_dy = np.array([[np.cos(phi_dy), -np.sin(phi_dy)],
                              [np.sin(phi_dy), np.cos(phi_dy)]])
+
             # calculate vector to object relative to body
+            # TODO: Change obj vector by determining the closest to shapely polygon
+            # TODO:  obs = closest_pt(obstacle, y)
+
             obj_vec = obstacle - y
             # rotate it by the direction we're going
             obj_vec = np.dot(R_dy, obj_vec)
@@ -131,5 +173,5 @@ def avoid_obstacles(y, dy, goal, obstacles=np.array([[5.6, 6.6], [4.0, 8.0]]), b
                 pval = 0
 
             p += pval
-    print("p is: ", p)
+    # print("p is: ", p)
     return p
