9e1504873f7816f8394a96d61624e00af5cb969e
gutsy-robot <agrawalsiddharth12@gmail.com>
2019-03-08 18:44:43 -0500

    works on py3, round off error on 2.7

---
+++ pydmps/grid_search.py
@@ -0,0 +1,217 @@
+"""
+Dijkstra grid based planning
+author: Atsushi Sakai(@Atsushi_twi)
+"""
+
+import matplotlib.pyplot as plt
+import math
+from shapely.geometry.polygon import LinearRing, Polygon
+from shapely.geometry import Point, mapping
+
+
+show_animation = True
+
+
+class Node(object):
+
+    def __init__(self, x, y, cost, pind):
+        self.x = x
+        self.y = y
+        self.cost = cost
+        self.pind = pind
+
+    def __str__(self):
+        return str(self.x) + "," + str(self.y) + "," + str(self.cost) + "," + str(self.pind)
+
+
+def dijkstra_planning(sx, sy, gx, gy, obstacles, reso):
+    """
+
+    :param sx: start x coordinate
+    :param sy: start y coordinate
+    :param gx: goal x coordinate
+    :param gy: goal y coordinate
+    :param obstacles: list of shapely polygons
+    :param reso: resolution of smaller grids
+    :return: time parameterised path
+    """
+
+    nstart = Node(round(sx / reso), round(sy / reso), 0.0, -1)
+    ngoal = Node(round(gx / reso), round(gy / reso), 0.0, -1)
+
+    # ox = [iox / reso for iox in ox]
+    # oy = [ioy / reso for ioy in oy]
+
+    obmap, minx, miny, maxx, maxy, xw, yw = calc_obstacle_map(obstacles, reso)
+
+    motion = get_motion_model()
+
+    openset, closedset = dict(), dict()
+    openset[calc_index(nstart, xw, minx, miny)] = nstart
+
+    while 1:
+        c_id = min(openset, key=lambda o: openset[o].cost)
+        current = openset[c_id]
+        #  print("current", current)
+
+        # show graph
+        if show_animation:
+            plt.plot(current.x * reso, current.y * reso, "xc")
+            if len(closedset.keys()) % 10 == 0:
+                plt.pause(0.001)
+
+        if current.x == ngoal.x and current.y == ngoal.y:
+            print("Find goal")
+            ngoal.pind = current.pind
+            ngoal.cost = current.cost
+            break
+
+        # Remove the item from the open set
+        del openset[c_id]
+        # Add it to the closed set
+        closedset[c_id] = current
+
+        # expand search grid based on motion model
+        for i, _ in enumerate(motion):
+            node = Node(current.x + motion[i][0], current.y + motion[i][1],
+                        current.cost + motion[i][2], c_id)
+            n_id = calc_index(node, xw, minx, miny)
+
+            if not verify_node(node, obmap, minx, miny, maxx, maxy):
+                continue
+
+            if n_id in closedset:
+                continue
+            # Otherwise if it is already in the open set
+            if n_id in openset:
+                if openset[n_id].cost > node.cost:
+                    openset[n_id].cost = node.cost
+                    openset[n_id].pind = c_id
+            else:
+                openset[n_id] = node
+
+    rx, ry = calc_final_path(ngoal, closedset, reso)
+
+    return rx, ry
+
+
+def calc_final_path(ngoal, closedset, reso):
+    # generate final course
+    rx, ry = [ngoal.x * reso], [ngoal.y * reso]
+    pind = ngoal.pind
+    while pind != -1:
+        n = closedset[pind]
+        rx.append(n.x * reso)
+        ry.append(n.y * reso)
+        pind = n.pind
+
+    return rx, ry
+
+
+def verify_node(node, obmap, minx, miny, maxx, maxy):
+
+    print("nodex and nodey are: ", (node.x, node.y))
+    if obmap[node.x][node.y]:
+        return False
+
+    if node.x < minx:
+        return False
+    elif node.y < miny:
+        return False
+    elif node.x > maxx:
+        return False
+    elif node.y > maxy:
+        return False
+
+    return True
+
+
+def calc_obstacle_map(obstacles, reso):
+
+    minx = 0
+    miny = 0
+    maxx = 14
+    maxy = 14
+    #  print("minx:", minx)
+    #  print("miny:", miny)
+    #  print("maxx:", maxx)
+    #  print("maxy:", maxy)
+
+    xwidth = round(maxx - minx)
+    ywidth = round(maxy - miny)
+    print("xwidth:", xwidth)
+    print("ywidth:", ywidth)
+
+    # obstacle map generation
+    obmap = [[False for i in range(xwidth)] for i in range(ywidth)]
+    for ix in range(xwidth):
+        x = ix + minx
+        for iy in range(ywidth):
+            y = iy + miny
+            #  print(x, y)
+            point = Point((x, y))
+            for obstacle in obstacles:
+                if obstacle.contains(point):
+                    obmap[ix][iy] = True
+                    break
+
+    return obmap, minx, miny, maxx, maxy, xwidth, ywidth
+
+
+def calc_index(node, xwidth, xmin, ymin):
+    return (node.y - ymin) * xwidth + (node.x - xmin)
+
+
+def get_motion_model():
+    # dx, dy, cost
+    motion = [[1, 0, 1],
+              [0, 1, 1],
+              [-1, 0, 1],
+              [0, -1, 1],
+              [-1, -1, math.sqrt(2)],
+              [-1, 1, math.sqrt(2)],
+              [1, -1, math.sqrt(2)],
+              [1, 1, math.sqrt(2)]]
+
+    return motion
+
+
+def main():
+    print(__file__ + " start!!")
+
+    # start and goal position
+    sx = 10.0  # [m]
+    sy = 10.0  # [m]
+    gx = 50.0  # [m]
+    gy = 50.0  # [m]
+    grid_size = 1.0  # [m]
+
+    coords = [(6.0, 2.0), (6.0, 6.0), (8.0, 6.0), (8.0, 2.0)]
+    poly1 = Polygon(coords)
+
+    coords2 = [(6.0, 6.5), (6.0, 8.0), (8.0, 8.0), (8.0, 6.5)]
+    poly2 = Polygon(coords2)
+
+    obstacles = [poly1, poly2]
+
+    if show_animation:
+        # plt.plot(ox, oy, ".k")
+
+        # plot shapely polygons here:
+        for obstacle in obstacles:
+            x, y = obstacle.exterior.xy
+            plt.plot(x, y, color='#6699cc', alpha=0.7, linewidth=3, solid_capstyle='round', zorder=2)
+
+        plt.plot(sx, sy, "xr")
+        plt.plot(gx, gy, "xb")
+        plt.grid(True)
+        plt.axis("equal")
+
+    rx, ry = dijkstra_planning(sx, sy, gx, gy, obstacles, grid_size)
+
+    if show_animation:
+        plt.plot(rx, ry, "-r")
+        plt.show()
+
+
+main()
